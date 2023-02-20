c0aadb6e18da15a4357567eb0e9bb63d0e4244a4
Nils <nils-stegemann@gmx.de>
2023-02-20 17:52:48 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/nils1904/weather_station/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nils1904.weather_station
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const themColor = Color(0x40F30606);
